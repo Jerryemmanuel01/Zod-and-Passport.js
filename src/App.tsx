cba197e29196097d98e06698584cf4206cc60b15
Jerryemmanuel01 <jerryemmanuelolisa@gmail.com>
2024-04-12 19:44:03 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <>
+      <h2>Zod and Passport Js</h2>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Zod.tsx
@@ -0,0 +1,15 @@
+
+const Zod = () => {
+  return (
+    <div>
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Jerryemmanuel01/Zod-and-Passport.js.git
+git push -u origin main
+    </div>
+  )
+}
+
+export default Zod
